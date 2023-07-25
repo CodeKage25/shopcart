07ebbe3cf781615ed5744d63ea96e70c60c3b9d7
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2023-07-25 16:17:54 +0100

    amazon cloud storage update moved  to the product model

--- server/src/models/products.model.js
+++ server/src/models/products.model.js
@@ -2,3 +2,22 @@ const fs = require("fs");
 const path = require("path");
 const { parse } = require('csv-parse');
 
+require('dotenv').config();
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const connectionString = connectionString;
+const containerName = "shopcart";
+const blobName = "productData.json";
+
+// Fetch JSON Data from Azure Blob Storage using Azure Storage SDK
+async function fetchJsonData() {
+  const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  const response = await blockBlobClient.download(0); // Start reading from the beginning
+  const jsonData = await response.read();
+  return JSON.parse(jsonData.toString());
+}
+
+module.exports = fetchJsonData;
