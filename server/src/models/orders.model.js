463f6e3c7aa67a33f22b764194dc31ff22048183
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2023-07-29 15:29:45 +0100

    update to the order endpoint

--- server/src/models/orders.model.js
+++ server/src/models/orders.model.js
@@ -4,13 +4,28 @@ const orderDatabase = require('./orders.mongo')
 // function to create a new order
 async function createOrder(orderData) {
     try {
-        const order = new orderDatabase(orderData)
-        await order.save();
-        return order;
-    } catch (error) {
-        console.error('Error creating order:', error)
-        throw error;
+    // Calculate the total price based on the products' currentPrice and quantity
+    let totalPrice = 0;
+    for (const product of orderData.products) {
+      totalPrice += product.currentPrice * product.quantity;
     }
+
+    // Create a new order document with the necessary fields
+    const order = new orderDatabase({
+      products: orderData.products.map((product) => product.productId), // Extracting product IDs
+      totalPrice,
+      customerName: orderData.customerName,
+      customerEmail: orderData.customerEmail,
+    });
+
+    // Save the order to the database
+    await order.save();
+
+    return order;
+  } catch (error) {
+    console.error('Error creating order:', error);
+    throw error;
+  }
 }
 
 
